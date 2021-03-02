00884db32f1fe6451f284570ec8d623edb266b35
psthejohn <36690315+psthejohn@users.noreply.github.com>
2021-03-02 21:56:20 +0530

    Update home.js

--- src/Pages/home/home.js
+++ src/Pages/home/home.js
@@ -3,7 +3,11 @@ import './home.css';
 function Home() {
   return (
     <div>
-        Hello world
+      Hii, I am Pushpinder Singh.
+      A Professional Front End Developer.
+      <br>
+    <button>Hire Me<button>
+    Ph: +91-9610226367
     </div>
   );
 }
