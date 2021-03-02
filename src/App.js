fbe34ca5d32bc14f7b9a44d3e4ac4c926a5d169a
psthejohn <psthejohn@gmail.com>
2021-03-02 21:13:36 +0530

    Adding Files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './Pages/home/home.js';
+
+function App() {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/home/home.js
@@ -0,0 +1,11 @@
+import './home.css';
+
+function Home() {
+  return (
+    <div>
+        Hello world
+    </div>
+  );
+}
+
+export default Home;
